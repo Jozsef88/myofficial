cb269d10462263bf227dcd3e2754b1a40cbf1414
Jozsef88 <49954645+Jozsef88@users.noreply.github.com>
2020-04-03 09:07:29 +0100

    Updates

---
+++ precache-manifest.f13d6301b9c9cee2884c7f1d0d589f04.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d17ab78781ba9ec65d8e3574f3df6d56",
+    "url": "/myofficial/index.html"
+  },
+  {
+    "revision": "ff6efa601712eb1719b4",
+    "url": "/myofficial/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4dda13f310b35b4db624",
+    "url": "/myofficial/static/css/main.285978b0.chunk.css"
+  },
+  {
+    "revision": "ff6efa601712eb1719b4",
+    "url": "/myofficial/static/js/2.288db395.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myofficial/static/js/2.288db395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dda13f310b35b4db624",
+    "url": "/myofficial/static/js/main.d617488b.chunk.js"
+  },
+  {
+    "revision": "601de0c16dcb843eab74",
+    "url": "/myofficial/static/js/runtime-main.ab9ed185.js"
+  },
+  {
+    "revision": "4ce99283e17564bd14aaa100e39c680e",
+    "url": "/myofficial/static/media/author-image.4ce99283.jpg"
+  },
+  {
+    "revision": "139e74e298bca37a25d2bd5868552e04",
+    "url": "/myofficial/static/media/fontawesome-webfont.139e74e2.svg"
+  },
+  {
+    "revision": "706450d7bba6374ca02fe167d86685cb",
+    "url": "/myofficial/static/media/fontawesome-webfont.706450d7.ttf"
+  },
+  {
+    "revision": "97493d3f11c0a3bd5cbd959f5d19b699",
+    "url": "/myofficial/static/media/fontawesome-webfont.97493d3f.woff2"
+  },
+  {
+    "revision": "d9ee23d59d0e0e727b51368b458a0bff",
+    "url": "/myofficial/static/media/fontawesome-webfont.d9ee23d5.woff"
+  },
+  {
+    "revision": "f7c2b4b747b1a225eb8dee034134a1b0",
+    "url": "/myofficial/static/media/fontawesome-webfont.f7c2b4b7.eot"
+  },
+  {
+    "revision": "75a11cf9bd9cd916260acc0c68231e29",
+    "url": "/myofficial/static/media/left-image.75a11cf9.jpg"
+  },
+  {
+    "revision": "7f9ca34307033a1db23f979ffafb000f",
+    "url": "/myofficial/static/media/page-bg.7f9ca343.jpg"
+  },
+  {
+    "revision": "e6d82ef96691791ca5172fae8017f00a",
+    "url": "/myofficial/static/media/right-image.e6d82ef9.jpg"
+  }
+]);
